6f055b2accc78fe8dfd52b19263204042b569620
fiorella <difiore8fiorella@gmail.com>
2026-02-11 11:01:58 -0300

    agregando cambios

--- src/app/vivero-home/vivero-home.component.ts
+++ src/app/vivero-home/vivero-home.component.ts
@@ -12,9 +12,9 @@ import { Component } from '@angular/core';
 export class ViveroHomeComponent {
   imagenes=
     {
-      image1: "https://i.ibb.co/8nrXXZ1v/carousel1.jpg",
-      image2: "https://i.ibb.co/20xdVghP/carousel2.jpg",
-      image3: "https://i.ibb.co/cc6LS8Jj/carousel3.jpg",
+      image1: "https://i.ibb.co/Ndvz6W7J/carousel1-1.png",
+      image2: "https://i.ibb.co/DHCGNb9p/carousel2-2.png",
+      image3: "https://i.ibb.co/chrzhbKz/carousel3-1.png",
       image4: "https://i.ibb.co/RkkpyY1B/mediodepago.png",
       image5: "https://i.ibb.co/XB0LzqJ/image5.jpg",
       image6: "https://i.ibb.co/F43cGHDY/image6.jpg",
