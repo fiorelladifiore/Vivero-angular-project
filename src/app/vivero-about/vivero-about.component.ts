1838d77913532db678c8183a82f7ab53148a4768
fiorella <difiore8fiorella@gmail.com>
2026-03-02 14:08:32 -0300

    arreglos en el about

--- src/app/vivero-about/vivero-about.component.ts
+++ src/app/vivero-about/vivero-about.component.ts
@@ -10,6 +10,9 @@ import { Component } from '@angular/core';
 export class ViveroAboutComponent {
   imagen=
     {
-      image1: "https://i.ibb.co/Jwkqydhg/quienes-Somos.png"
+      image1: "https://i.ibb.co/dJfTBFGJ/quienssomos1.jpg",
+      image2: "https://i.ibb.co/qYGYg54T/quienessomos2.jpg",
+      image3: "https://i.ibb.co/ymkff1jJ/quienessomos3.jpg",
+      image4: "https://i.ibb.co/nNdsXZLd/leavesbackground.jpg"
     }
 }
